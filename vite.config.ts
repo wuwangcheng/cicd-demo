ca68cc2b34a205c88b7d0ecb84a9069ae3e017ec
wuwangcheng <1461006743@qq.com>
2024-09-10 23:52:10 +0800

    配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cicd-demo',
   plugins: [vue()],
+  build:{
+    rollupOptions:{
+      output:{
+        manualChunks(id){
+          if(id.includes('node_modules')){
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        }
+      }
+    }
+  }
 })
